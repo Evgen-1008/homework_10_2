ca2e811510bbbe42d5f815813c6b2df248b48c6b
Evgen-1008 <evgeniy.sakriev@mail.ru>
2025-12-23 13:09:30 +0500

    Add files via upload

--- project/tests/test_get_mask_card_number.py
+++ project/tests/test_get_mask_card_number.py
@@ -1,5 +1,5 @@
 from src.masks import get_mask_card_number
 
 
-def test_get_mask_cart_number(mask_card_number):
+def test_get_mask_card_number(mask_card_number):
     assert get_mask_card_number("7556 7748 8893 8885") == mask_card_number
